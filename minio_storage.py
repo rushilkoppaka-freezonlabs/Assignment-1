30ddf88b689e3d680cba503776c579c627766f35
rushilkoppaka-freezonlabs <83945787+rushilkoppaka-freezonlabs@users.noreply.github.com>
2021-05-13 17:10:50 +0530

    Update minio_storage.py

--- minio_storage.py
+++ minio_storage.py
@@ -1,25 +1,23 @@
 from minio import Minio
 from minio.error import S3Error
-import read_mail as rushil
+
 
 import io
 
-def listToString(s): 
-    
+
+def listToString(s):
     # initialize an empty string
-    str1 = "" 
-    
-    # traverse in the string  
-    for ele in s: 
-        str1 += ele  
-    
-    # return string  
-    return str1 
-        
+    str1 = ""
+
+    # traverse in the string
+    for ele in s:
+        str1 += ele
+
+        # return string
+    return str1
 
 
-
-def main():
+def main(New_mail):
     # Create a client with the MinIO server playground, its access key
     # and secret key.
     client = Minio(
@@ -27,8 +25,8 @@ def main():
         access_key="Q3AM3UQ867SPQQA43P2F",
         secret_key="zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
     )
-    
-    inbox, doc =rushil.read_mail()
+
+    inbox, doc = New_mail
     """
     print(type(doc[0]))
     print(len(inbox)) 
@@ -38,23 +36,23 @@ def main():
     print(type(object))
     """
     for i in range(len(inbox)):
-        
+
         # Make 'asiatrip' bucket if not exist.
-        
+
         found = client.bucket_exists(inbox["username"][i].lower())
         if not found:
             client.make_bucket(inbox["username"][i].lower())
         else:
-            print("Bucket",inbox["username"][i],"already exists")
+            print("Bucket", inbox["username"][i], "already exists")
 
         # Upload '/home/user/Photos/asiaphotos.zip' as object name
         # 'asiaphotos-2015.zip' to bucket 'asiatrip'.
-       
-        #client.compose_object(inbox["username"][i], inbox['file name'][i], sources, sse=None, metadata=None, tags=None, retention=None, legal_hold=False)
-        result=client.put_object(inbox["username"][i].lower(),  listToString(inbox['file name'][i]).lower(),io.BytesIO(doc[i]), -1 , part_size=10*1024*1024)
+
+        # client.compose_object(inbox["username"][i], inbox['file name'][i], sources, sse=None, metadata=None, tags=None, retention=None, legal_hold=False)
+        result = client.put_object(inbox["username"][i].lower(), listToString(inbox['file name'][i]).lower(),
+                                   io.BytesIO(doc[i]), -1, part_size=10 * 1024 * 1024)
         print("the file", i, "  is uploaded")
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     main()
-  
