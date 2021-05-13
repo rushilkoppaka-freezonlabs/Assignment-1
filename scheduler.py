cd0d7eb44d0367f001bfa92cfb0ee6ae910f1116
rushilkoppaka-freezonlabs <83945787+rushilkoppaka-freezonlabs@users.noreply.github.com>
2021-05-13 12:35:11 +0530

    Add files via upload
    This will check for new messages in Gmail. If received will call read_mail and extract new messages as a DataFrame. This DataFrame will need to be pushed to the minion Server

---
+++ scheduler.py
@@ -0,0 +1,41 @@
+import read_mail as read
+from imapclient import IMAPClient
+
+HOST = 'imap.gmail.com'
+username = "abcd12ghijk@gmail.com"
+password = "abcd@1234"
+
+def scheduler():
+        server = IMAPClient(HOST)
+        server.login(username,password)
+        server.select_folder('inbox')
+
+        # Start IDLE mode
+        server.idle()
+        print("Connection is now in IDLE mode, send yourself an email or quit with ^c")
+
+        while True:
+            try:
+                # Wait for up to 30 seconds for an IDLE response
+                response= server.idle_check(timeout=5)
+                if response != [] :
+                    response=  response[0][1]
+
+                if response == b'EXISTS':
+                    new_email = read.read_mail()
+                    print(new_email)
+                    #now push this new email to minio
+
+                else :
+                    print("No New Messages Yet...")
+
+            except KeyboardInterrupt:
+                break
+
+        server.idle_done()
+        print("\nIDLE mode done")
+        server.logout()
+
+if __name__ == '__main__':
+    scheduler()
+
