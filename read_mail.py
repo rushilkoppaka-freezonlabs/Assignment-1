e3716265e34b59440f2ff4d1f76d8eb21b8f8bf8
rushilkoppaka-freezonlabs <83945787+rushilkoppaka-freezonlabs@users.noreply.github.com>
2021-05-13 09:20:52 +0530

    Update read_mail.py

--- read_mail.py
+++ read_mail.py
@@ -22,7 +22,7 @@ def read_mail():
     for num in search_data[0].split():
         _, data = mail.fetch(num, '(RFC822)')
         _, b = data[0]
-
+        #extracting one email
         email_message = email.message_from_bytes(b)
         if 'invoice' in email_message['Subject']:
             email_data = {'attachment': [], 'file name': [], 'Document type': []}
@@ -31,26 +31,22 @@ def read_mail():
                 if part.get_content_type() == "text/plain":
                     body = part.get_payload(decode=True)
                     email_data['Body'] = body.decode()
-                    continue
                 if part.get_content_maintype() == 'multipart':
                     continue
                 if part.get('Content-Disposition') is None:
                     continue
                 file_name = part.get_filename()
                 if bool(file_name):
-
                     encoded_string = str(part.get_payload(decode=True)).encode()
                     bytearray_string = bytearray(encoded_string)
                     email_data['attachment'].append(bytearray_string)
 
                     email_data['file name'].append(file_name)
                     email_data['Document type'].append(file_name.split('.')[1])
-                    # file_path = attachment_dir + '/' + file_name
-                    # with open(file_path, 'wb') as f:
-                    #     f.write(part.get_payload(decode=True))
+
                     for header in ['Subject', 'To', 'From', 'Date']:
                         email_data[header] = email_message[header]
-                    email_data['username'], email_data['email ID'] = email_message['From'].rsplit(' ',1)
+                    email_data['username'],_, email_data['email ID'] = email_message['From'].rsplit(' ',)
                     all_email.append(email_data)
 
     data_frame = pd.DataFrame(all_email)
@@ -65,3 +61,4 @@ def read_mail():
 if __name__ == '__main__':
     inbox = read_mail()
     print(inbox)
+
