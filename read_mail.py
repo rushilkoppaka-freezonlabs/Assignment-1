41444de4e977576938a93414d30cf10c690bf940
rushilkoppaka-freezonlabs <83945787+rushilkoppaka-freezonlabs@users.noreply.github.com>
2021-05-13 18:11:30 +0530

    Update read_mail.py

--- read_mail.py
+++ read_mail.py
@@ -7,14 +7,12 @@ host = 'imap.gmail.com'
 username = "abcd12ghijk@gmail.com"
 password = "abcd@1234"
 
-attachment_dir = "/Users/rushilkoppaka/PycharmProjects/freezonlabs_internship"
-
-
 def read_mail():
     mail = imaplib.IMAP4_SSL(host)
     mail.login(username, password)
     mail.select("inbox")
-
+    
+    #searching mailbox for unseen mails.
     _, search_data = mail.search(None, 'UNSEEN')
     attachment=[]
     all_email = []
@@ -24,18 +22,26 @@ def read_mail():
         _, b = data[0]
         #extracting one email
         email_message = email.message_from_bytes(b)
+        
+        #checking for 'invoice' in subject
         if 'invoice' in email_message['Subject']:
             email_data = {'attachment': [], 'file name': [], 'Document type': []}
 
             for part in email_message.walk():
+                #getting body(multipart) of the mail
                 if part.get_content_type() == "text/plain":
                     body = part.get_payload(decode=True)
                     email_data['Body'] = body.decode()
+                    
                 if part.get_content_maintype() == 'multipart':
                     continue
+                    
                 if part.get('Content-Disposition') is None:
                     continue
+                    
+                #Checking if file and attached and getting the file name of attachment    
                 file_name = part.get_filename()
+                
                 if bool(file_name):
                     encoded_string = str(part.get_payload(decode=True)).encode()
                     bytearray_string = bytearray(encoded_string)
@@ -44,17 +50,19 @@ def read_mail():
 
                     email_data['file name'].append(file_name)
                     email_data['Document type'].append(file_name.split('.')[1])
-
+                    
+                    #getting rest of the details of the mail
                     for header in ['Subject', 'To', 'From', 'Date']:
                         email_data[header] = email_message[header]
+                    
                     email_data['username'],_, email_data['email ID'] = email_message['From'].rsplit(' ',)
                     all_email.append(email_data)
 
     data_frame = pd.DataFrame(all_email)
+    #returning DataFrame as well as list of bytearray of all attachments
     return data_frame, attachment
 
 
-# DataFrame columns format -> attachment, Subject, To, From, Date
 # There maybe multiple attachments for single mail
 # Each attachment will have its own row with all values.
 
